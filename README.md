b11beab343f7d7b846057ff9072663bd27aff93f
Jason Livesay <runvnc@gmail.com>
2016-02-06 12:56:31 -0800

    checked

--- README.md
+++ README.md
@@ -13,16 +13,16 @@ import {callInterval, rateLimited} from 'ratelimited';
 
 callInterval('API_1_A', 200); // wait at least 200 ms between function calls for key API_1_A
 
-rateLimited('API_1_A', api1ACall, param1, param2);  // immediately calls api1ACall(param1,param2)
-rateLimited('API_1_A', api1AOther, param1, param2); // waits 200ms then calls api1AOther(param1, param2)
+rateLimited('API_1_A', api1ACall, 'param1', 'param2');  // immediately calls api1ACall(param1,param2)
+rateLimited('API_1_A', api1AOther, 'param1', 'param2'); // waits 200ms then calls api1AOther(param1, param2)
 
-rateLimited('API_2_B', api2BCall, arg1, arg2, arg3, arg4); // immediately calls api2BCall(arg1,arg2,arg3,arg4)
-rateLimited('API_2_B', api2BDiff, arg1); // waits 1000 ms (1 second) then calls api2BDiff(arg1);
+rateLimited('API_2_B', api2BCall, 'arg1', 'arg2', 'arg3', 'arg4'); // immediately calls api2BCall(arg1,arg2,arg3,arg4)
+rateLimited('API_2_B', api2BDiff, 'arg1'); // waits 1000 ms (1 second) then calls api2BDiff(arg1);
 
 // -- Actual order of calls for the above example:
 // apiACall() at 0 ms
 // api2BCall() at ~0 ms
-// api1AOther() at 200 ms
-// api2BDiff() at 1000 ms
+// api1AOther() at ~200 ms
+// api2BDiff() at ~1000 ms
 
 ```
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,27 @@
 import {callInterval, rateLimited} from '../ratelimited';
 
 function api1ACall(p1, p2, p3) {
-  console.log('api1ACall', p1, p2, p3);
+  console.log(new Date().getTime()-start,' api1ACall', p1, p2, p3);
 }
 
-rateLimited('API_1_A', api1ACall, 'param1');
-rateLimited('API_1_A', api1ACall, 'param1', 'param2');
-rateLimited('API_1_A', api1ACall, 'param1', 'param2', 'param3');
+function api1AOther(p1, p2, p3) {
+  console.log(new Date().getTime()-start,' api1AOther', p1, p2, p3);
+}
+
+function api2BCall(p1, p2, p3, p4) {
+  console.log(new Date().getTime()-start, 'api2BCall', p1, p2, p3, p4);
+}
+
+function api2BDiff(p1, p2, p3) {
+  console.log(new Date().getTime()-start, 'api2BDiff', p1, p2, p3);
+}
+
+let start = new Date().getTime();
+
+callInterval('API_1_A', 200); // wait at least 200 ms between function calls for key API_1_A
+rateLimited('API_1_A', api1ACall, 'param1', 'param2');  // immediately calls api1ACall(param1,param2)
+rateLimited('API_1_A', api1AOther, 'param1', 'param2'); // waits 200ms then calls api1AOther(param1, param2)
+            
+rateLimited('API_2_B', api2BCall, 'arg1', 'arg2', 'arg3', 'arg4'); // immediately calls api2BCall(arg1,arg2,arg3,arg4)
+rateLimited('API_2_B', api2BDiff, 'arg1'); // waits 1000 ms (1 second) then calls api2BDiff(arg1);
 
